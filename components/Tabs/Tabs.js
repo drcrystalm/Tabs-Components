c22424aba3d2cad089d95a86c4ffe5235ef8283c
Crystal Martin <drcrystalm@gmail.com>
2019-05-05 13:23:02 -0500

    made suggested changes, tabs still don't work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,14 +8,15 @@ class TabLink {
 
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(
-      `.tab-item[data-tab="${this.data}"]`
+      `.tabs-item[data-tab="${this.data}"]`
     );
 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    //Why itemElement???
-    // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', this.select(this));
+    //Why itemElement???because itemElement refers to the specific data-tab of the tabItem we are looking for. so when we click on the link, it will refer to the correct content. ALSO CRYSTAL- because if you looked at the instructions, that's what it said to do:)
+
+    // Add a click event listener on this instance, calling the select method on clickS
+    this.itemElement.addEventListener('click', () => this.select());
   };
 
   select() {
@@ -28,13 +29,10 @@ class TabLink {
       tab.classList.remove("tabs-link-selected")
     );
     // Need to learn how to do this from scratch... Need practice
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach(link => {
-    //   document.querySelectorAll('.tabs-link-selected').remove
-    // });
+
 
     // Add a class named "tabs-link-selected" to this link
-    this.element = tab.classList.add("tabs-link-selected");
+    this.element.classList.add("tabs-link-selected");
 
     // Call the select method on the item associated with this link
     //NEED TO FIGURE THIS OUT>>> DON'T UNDERSTAND
