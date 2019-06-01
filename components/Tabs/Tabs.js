6b7f29f98b338c7af3136db966cec6dbef69db2f
Crystal Martin <drcrystalm@gmail.com>
2019-05-31 21:34:54 -0500

    changed the typo tabs still do not work

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,62 +1,60 @@
 class TabLink {
-  constructor(element) {
-    // Assign this.element to the passed in DOM element
-    this.element = element;
+    constructor(element) {
+        // Assign this.element to the passed in DOM element
+        this.element = element
 
-    // Get the custom data attribute on the Link
-    this.data = this.element.dataset.tab;
+        // Get the custom data attribute on the Link
+        this.data = this.element.dataset.tab
 
-    // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(
-      `.tabs-item[data-tab="${this.data}"]`
-    );
+        // Using the custom data attribute get the associated Item element
+        this.itemElement = document.querySelector(
+            `.tabs-item[data-tab="${this.data}"]`
+        )
 
-    // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem(this.itemElement);
-    //Why itemElement???because itemElement refers to the specific data-tab of the tabItem we are looking for. so when we click on the link, it will refer to the correct content. ALSO CRYSTAL- because if you looked at the instructions, that's what it said to do:)
+        // Using the Item element, create a new instance of the TabItem class
+        this.tabItem = new TabItem(this.itemElement)
+        //Why itemElement???because itemElement refers to the specific data-tab of the tabItem we are looking for. so when we click on the link, it will refer to the correct content. ALSO CRYSTAL- because if you looked at the instructions, that's what it said to do:)
 
-    // Add a click event listener on this instance, calling the select method on clickS
-    this.itemElement.addEventListener('click', () => this.select());
-  };
+        // Add a click event listener on this instance, calling the select method on clickS
+        this.itemElement.addEventListener("click", () => this.select())
+    }
 
-  select() {
-    // Get all of the elements with the tabs-link class
-    const links = document.querySelectorAll(".tabs-link");
+    select() {
+        // Get all of the elements with the tabs-link class
+        const links = document.querySelectorAll(".tabs-link")
 
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+        // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
 
-    Array.from(links).forEach(tab =>
-      tab.classList.remove("tabs-link-selected")
-    );
-    // Need to learn how to do this from scratch... Need practice
+        Array.from(links).forEach(tab =>
+            tab.classList.remove("tabs-link-selected")
+        )
+        // Need to learn how to do this from scratch... Need practice
 
+        // Add a class named "tabs-link-selected" to this link
+        this.element.classList.add("tabs-link-selected")
 
-    // Add a class named "tabs-link-selected" to this link
-    this.element.classList.add("tabs-link-selected");
-
-    // Call the select method on the item associated with this link
-    //NEED TO FIGURE THIS OUT>>> DON'T UNDERSTAND
-    this.tabItem.select(this.itemElement);
-  }
+        // Call the select method on the item associated with this link
+        //NEED TO FIGURE THIS OUT>>> DON'T UNDERSTAND
+        this.tabItem.select(this.itemElement)
+    }
 }
 
 class TabItem {
-  constructor(element) {
-    // Assign this.element to the passed in element
-    this.element = element;
-  }
+    constructor(element) {
+        // Assign this.element to the passed in element
+        this.element = element
+    }
 
-  select() {
-    // Select all ".tabs-item" elements from the DOM
-    const items = document.querySelectorAll(".tabs-item");
+    select() {
+        // Select all ".tabs-item" elements from the DOM
+        const items = document.querySelectorAll(".tabs-item")
 
-    // Remove the class "tabs-item-selected" from each element
-    items.forEach(item => item.classList.remove("tabs-item-selected"));
+        // Remove the class "tabs-item-selected" from each element
+        items.forEach(item => item.classList.remove("tabs-item-selected"))
 
-
-    // Add a class named "tabs-item-selected" to this element
-    this.element.classList.add("tabs-item-selected");
-  }
+        // Add a class named "tabs-item-selected" to this element
+        this.element.classList.add("tabs-item-selected")
+    }
 }
 
 /* START HERE: 
@@ -69,8 +67,8 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll(".tab-links");
+links = document.querySelectorAll(".tabs-link")
 
 links.forEach(link => {
-  new TabLink(link);
-});
+    new TabLink(link)
+})
